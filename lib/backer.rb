160c75ab4b1ceef3afe7850185d6d1acbcf8f462
Chris Kay <chriskay25@gmail.com>
2019-05-29 18:09:08 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,9 @@ class Backer
     @backed_projects = []
   end 
   
-  def back_project(title)
-    @backed_projects << title 
-    Project.backers << self 
+  def back_project(project)
+    @backed_projects << project 
+    project.backers << self 
   end 
   
 end 
